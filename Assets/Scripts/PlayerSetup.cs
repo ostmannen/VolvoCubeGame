e9f69d9b8ed0896cf3f6e57a704ccd4750e12e4d
ostmannen <60339614+ostmannen@users.noreply.github.com>
2026-02-28 11:31:33 +0100

    split screen

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSetup : MonoBehaviour
+{
+    public PlayerInput player1;
+    public PlayerInput player2;
+
+    void Start()
+    {
+        var gamepads = Gamepad.all;
+        if (gamepads.Count >= 2)
+        {
+            // Tilldela första gamepaden till player1
+            player1.SwitchCurrentControlScheme("Gamepad", gamepads[0]);
+            player1.ActivateInput();
+
+            // Tilldela andra gamepaden till player2
+            player2.SwitchCurrentControlScheme("Gamepad", gamepads[1]);
+            player2.ActivateInput();
+        }
+        else if (gamepads.Count >= 1)
+        {
+            player1.SwitchCurrentControlScheme("Gamepad", gamepads[0]);
+            player1.ActivateInput();
+
+            player2.ActivateInput();
+
+        }
+        else
+        {
+            Debug.LogWarning("Inte tillräckligt med gamepads anslutna.");
+        }
+    }
+}
